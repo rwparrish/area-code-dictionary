785a148205e31950f4dba480dab8040bb8c7b1c4
rwparrish <rino3717@gmail.com>
2020-12-07 13:03:49 -0700

    initialize method

--- practice.rb
+++ practice.rb
@@ -1,16 +1,23 @@
 require 'pry'
 
 class Dog
+  
   attr_accessor :name, :owner, :breed
 
-# classes by default have to_s method
-def to_s
-  "This #{@breed}'s first name is #{@name}"
-end
+  def initialize(name, owner, breed)
+    @name = name
+    @owner = owner
+    @breed = breed
+  end
 
-dog = Dog.new
-dog.name = "Fido"
-dog.breed = "Aussie"
-puts dog
+  # classes by default have to_s method
+  def to_s
+    "This #{@breed}'s first name is #{@name}"
+  end
+
+  dog = Dog.new
+  dog.name = "Fido"
+  dog.breed = "Aussie"
+  puts dog
 
 end
